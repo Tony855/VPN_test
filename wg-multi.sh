cff1d753317a2e85f286b5a753332886410cdb70
Tony855 <58850110+Tony855@users.noreply.github.com>
2025-03-18 16:19:12 +0700

    Update wg-multi.sh

--- wg-multi.sh
+++ wg-multi.sh
@@ -211,7 +211,7 @@ PublicKey = $(grep 'PrivateKey' "$CONFIG_DIR/$iface.conf" | awk '{print $3}' | w
 PresharedKey = $client_preshared
 Endpoint = $(echo "$public_ip" | tr -d '\r'):$(grep ListenPort "$CONFIG_DIR/$iface.conf" | awk '{print $3}')
 AllowedIPs = 0.0.0.0/0, ::/0
-PersistentKeepalive = 25
+PersistentKeepalive = 15
 EOF
 
     qrencode -t ansiutf8 < "$client_file"
